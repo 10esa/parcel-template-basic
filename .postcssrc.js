c870f66dea5b6fd547de5dff5201134c2bf5ad0a
10esa <maruovs@naver.com>
2021-10-05 20:33:56 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,16 @@
+// ESM
+// CommonJS // nodeJS환경의 경우
+
+// import autoprefixer from 'autoprefixer'
+const autoprefixer = require('autoprefixer')
+
+// export{
+//   plugins: [
+//     autoprefixer
+//   ]
+// }
+module.exports = {
+  plugins: [
+    autoprefixer
+  ]
+}
